78087a7e4503d4eb617875a71956f38fb209068f
Michael <kolosovmichal@gmail.com>
2019-02-12 13:30:21 +0200

    first commit

---
+++ Floydstriangle.java
@@ -0,0 +1,40 @@
+
+public class Floydstriangle {
+public static void main(String[] args) {
+	
+	int z =1;
+	for (int n = 1;n<=6;n++) {
+		for( int c = 0; c < n; c++) {  //пока  с < 1(n) -      ;
+			System.out.print(z+" ");  // - вывожу 1 раз    ;
+			z++;
+		}System.out.println();
+	}
+}
+}
+
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+		
