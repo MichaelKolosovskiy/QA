3988af69edcee32d62bb3c4de92cce0fb06070e9
MichaelKolosovskiy <47351272+MichaelKolosovskiy@users.noreply.github.com>
2019-02-27 17:56:49 +0200

    Update Floydstriangle.java

--- Floydstriangle.java
+++ Floydstriangle.java
@@ -4,37 +4,10 @@ public static void main(String[] args) {
 	
 	int z =1;
 	for (int n = 1;n<=6;n++) {
-		for( int c = 0; c < n; c++) {  //пока  с < 1(n) -      ;
-			System.out.print(z+" ");  // - вывожу 1 раз    ;
+		for( int c = 0; c < n; c++) { 
+			System.out.print(z+" "); 
 			z++;
 		}System.out.println();
 	}
 }
-}
-
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-		
+}	
